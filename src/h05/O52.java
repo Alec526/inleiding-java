8d1565e46b5bec5e761d2fe406a3cf20aa59a2ad
Alec526 <aarnaoutoglou@roc-dev.com>
2020-09-21 10:58:58 +0200

    Opdracht 5.2 afgemaakt

---
+++ src/h05/O52.java
@@ -0,0 +1,54 @@
+package h05;
+
+import java.applet.Applet;
+import java.awt.*;
+
+public class O52 extends Applet {
+
+    int jeroen, valerie, hans;
+
+
+    public void init() {
+        jeroen = 100;
+        valerie = 80;
+        hans = 40;
+
+    }
+
+
+    public void paint(Graphics g) {
+        g.drawString("Gewicht", 160, 60);
+
+        g.drawString("0 kg", 50, 290);
+        g.drawString("20 kg",45, 250);
+        g.drawString("40 kg", 45, 210);
+        g.drawString("60 kg", 45, 170);
+        g.drawString("80 kg", 45, 130);
+        g.drawString("100 kg", 40, 90);
+
+        g.drawLine(80, 80, 80, 280);
+        g.drawLine(80, 280, 280, 280);
+
+        g.drawLine(80, 240, 280, 240);
+        g.drawLine(80, 200, 280, 200);
+        g.drawLine(80, 160, 280, 160);
+        g.drawLine(80, 120, 280, 120);
+        g.drawLine(80, 80, 280, 80);
+
+        g.setColor(Color.red);
+        g.fillRect(110, 200, 12, 80);
+        g.drawString("Valerie", 100, 300);
+        g.drawString("40 kg", 105, 320);
+
+        g.setColor(Color.GREEN);
+        g.fillRect(170, 120, 12, 160);
+        g.drawString("Hans", 160, 300);
+        g.drawString("80 kg", 160, 320);
+
+        g.setColor(Color.BLUE);
+        g.fillRect(230, 80, 12, 200);
+        g.drawString("Jeroen", 220, 300);
+        g.drawString("100 kg", 225, 320);
+
+    }
+}
