07b61ead101404c8f5ef310f6c867855a8736045
Alec526 <aarnaoutoglou@roc-dev.com>
2020-10-08 11:02:12 +0200

    Opdracht 10.2 afgemaakt

--- src/h10/O102.java
+++ src/h10/O102.java
@@ -7,8 +7,8 @@ import java.awt.event.*;
 public class O102 extends Applet {
 
     TextField tekstvak;
-    int hoogste, laagste, getal;
-    String tekst;
+    int hoogste, laagste, getal, getal2;
+    String tekst, tekst2;
 
 
 
@@ -16,6 +16,7 @@ public class O102 extends Applet {
         tekstvak = new TextField();
         tekstvak.addActionListener(new vak());
         tekst = "";
+        tekst2 = "";
 
         add(tekstvak);
 
@@ -24,6 +25,7 @@ public class O102 extends Applet {
 
     public void paint(Graphics g) {
         g.drawString(tekst, 50, 50);
+        g.drawString(tekst2, 50, 65);
 
     }
 
@@ -33,14 +35,16 @@ public class O102 extends Applet {
 
             s = tekstvak.getText();
             getal = Integer.parseInt(s);
+            getal2 = Integer.parseInt(s);
+
             if (getal > hoogste) {
                 hoogste = getal;
                 tekst = "hoogste getal tot nu toe is: " +hoogste;
                 repaint();
             }
-            if (getal > laagste) {
+            if (getal < hoogste) {
                 laagste = getal;
-                tekst = "laagste getal tot nu toe is: " +laagste;
+                tekst2 = "laagste getal tot nu toe is: " +laagste;
                 repaint();
             }
 
