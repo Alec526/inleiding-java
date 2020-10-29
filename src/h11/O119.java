80c7c6b2b21a9e38c5821a8ade7a6a5bbd262789
Alec526 <aarnaoutoglou@roc-dev.com>
2020-10-29 14:50:56 +0100

    H11.9 optimized

--- src/h11/O119.java
+++ src/h11/O119.java
@@ -7,7 +7,7 @@ public class O119 extends Applet {
 
 
     public void init() {
-        setSize(800, 800);
+        setSize(400, 400);
 
     }
 
@@ -19,117 +19,34 @@ public class O119 extends Applet {
         int b = 20;
         int h = 20;
 
-        for(int kolom = 0; kolom < 8; kolom++) {
-
-            if (kolom == 0 || kolom == 2 || kolom == 4 || kolom == 6) {
-                g.setColor(Color.black);
-                g.fillRect(x, y, b, h);
-            } else {
-                g.setColor(Color.white);
-                g.fillRect(x, y, b, h);
-            }
-            x += b;
-        }
-        x = 50;
-        y += h;
 
         for (int kolom = 0; kolom < 8; kolom++) {
-
-            if (kolom == 0 || kolom == 2 || kolom == 4 || kolom == 6) {
-                g.setColor(Color.white);
-                g.fillRect(x, y, b, h);
-            } else {
-                g.setColor(Color.black);
-                g.fillRect(x, y, b, h);
+            for (int kolom2 = 0; kolom2 < 8; kolom2++) {
+                if (kolom % 2 == 0) {
+                    if (kolom2 % 2 == 0){
+                        g.setColor(Color.white);
+                        g.fillRect(x, y, b, h);
+                    } else {
+                        g.setColor(Color.black);
+                        g.fillRect(x, y, b, h);
+                    }
+                } else {
+                    if (kolom2 % 2 == 1){
+                        g.setColor(Color.white);
+                        g.fillRect(x, y, b, h);
+                    } else {
+                        g.setColor(Color.black);
+                        g.fillRect(x, y, b, h);
+                    }
+                }
+                x += b;
             }
-            x += b;
+            x = 50;
+            y += h;
+
+
         }
-        x = 50;
-        y += h;
 
-        for(int kolom = 0; kolom < 8; kolom++) {
-
-            if (kolom == 0 || kolom == 2 || kolom == 4 || kolom == 6) {
-                g.setColor(Color.black);
-                g.fillRect(x, y, b, h);
-            } else {
-                g.setColor(Color.white);
-                g.fillRect(x, y, b, h);
-            }
-            x += b;
-        }
-        x = 50;
-        y += h;
-
-        for (int kolom = 0; kolom < 8; kolom++) {
-
-            if (kolom == 0 || kolom == 2 || kolom == 4 || kolom == 6) {
-                g.setColor(Color.white);
-                g.fillRect(x, y, b, h);
-            } else {
-                g.setColor(Color.black);
-                g.fillRect(x, y, b, h);
-            }
-            x += b;
-        }
-        x = 50;
-        y += h;
-
-        for(int kolom = 0; kolom < 8; kolom++) {
-
-            if (kolom == 0 || kolom == 2 || kolom == 4 || kolom == 6) {
-                g.setColor(Color.black);
-                g.fillRect(x, y, b, h);
-            } else {
-                g.setColor(Color.white);
-                g.fillRect(x, y, b, h);
-            }
-            x += b;
-        }
-        x = 50;
-        y += h;
-
-        for (int kolom = 0; kolom < 8; kolom++) {
-
-            if (kolom == 0 || kolom == 2 || kolom == 4 || kolom == 6) {
-                g.setColor(Color.white);
-                g.fillRect(x, y, b, h);
-            } else {
-                g.setColor(Color.black);
-                g.fillRect(x, y, b, h);
-            }
-            x += b;
-        }
-        x = 50;
-        y += h;
-
-        for(int kolom = 0; kolom < 8; kolom++) {
-
-            if (kolom == 0 || kolom == 2 || kolom == 4 || kolom == 6) {
-                g.setColor(Color.black);
-                g.fillRect(x, y, b, h);
-            } else {
-                g.setColor(Color.white);
-                g.fillRect(x, y, b, h);
-            }
-            x += b;
-        }
-        x = 50;
-        y += h;
-
-        for (int kolom = 0; kolom < 8; kolom++) {
-
-            if (kolom == 0 || kolom == 2 || kolom == 4 || kolom == 6) {
-                g.setColor(Color.white);
-                g.fillRect(x, y, b, h);
-            } else {
-                g.setColor(Color.black);
-                g.fillRect(x, y, b, h);
-            }
-            x += b;
-        }
-        x = 50;
-        y += h;
 
     }
 }
