d9d784686b31e07a2f723e13f82f0a5a5a438eba
Alec526 <aarnaoutoglou@roc-dev.com>
2020-11-03 14:50:30 +0100

    H12.3 test

---
+++ src/h12/O123.java
@@ -0,0 +1,17 @@
+package h12;
+
+import java.applet.Applet;
+import java.awt.*;
+
+public class O123 extends Applet {
+
+
+    public void init() {
+
+    }
+
+
+    public void paint(Graphics g) {
+
+    }
+}
