3a3d00bf45b6bdc1c8ee5b5ce3ab5b075e88c87c
Alec526 <aarnaoutoglou@roc-dev.com>
2020-11-10 22:07:24 +0100

    H13.2 Afgemaakt

--- src/h13/O132.java
+++ src/h13/O132.java
@@ -11,21 +11,37 @@ public class O132 extends Applet {
 
     public void paint(Graphics g) {
 
-        tekenMuurBaksteen(g, 40, 40, 150, 40, 140, 110, 50);
+        tekenMuurBaksteen(g, 30, 30, 130, 100, 30, 170, 130, 240, 30, 310, 130, 380, 150, 40);
     }
 
-    public void tekenMuurBaksteen (Graphics g, int x1, int y1, int width, int height, int x3, int y3, int y4){
+    public void tekenMuurBaksteen (Graphics g, int x1, int y1, int x2, int y2, int x3, int y3, int x4, int y4, int x5, int y5, int x6, int y6, int width, int height){
+        setSize(1800, 500);
         setBackground(Color.gray);
-        for (int i = 0; i < 9; i++) {
-            g.setColor(Color.ORANGE);
+        for (int i = 0; i < 8; i++) {
+            g.setColor(Color.red);
             g.fillRect(x1, y1, width, height);
             x1 += 200;
         } for (int i = 0; i < 8; i++) {
-            g.setColor(Color.ORANGE);
+            g.setColor(Color.red);
+            g.fillRect(x2, y2, width, height);
+            x2 += 200;
+        } for (int i = 0; i < 8; i++) {
+            g.setColor(Color.red);
             g.fillRect(x3, y3, width, height);
             x3 += 200;
-
-            y4 += 250;
+        } for (int i = 0; i < 8; i++) {
+            g.setColor(Color.red);
+            g.fillRect(x4, y4, width, height);
+            x4 += 200;
+        } for (int i = 0; i < 8; i++) {
+            g.setColor(Color.red);
+            g.fillRect(x5, y5, width, height);
+            x5 += 200;
+        }
+        for (int i = 0; i < 8; i++) {
+            g.setColor(Color.red);
+            g.fillRect(x6, y6, width, height);
+            x6 += 200;
         }
 
     }
