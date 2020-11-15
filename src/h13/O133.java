374215654932ecbffe772df65cd20fa8356be4fc
Alec526 <aarnaoutoglou@roc-dev.com>
2020-11-15 13:07:55 +0100

    Praktijkopdracht H13 optimalisatie

--- src/h13/O133.java
+++ src/h13/O133.java
@@ -6,9 +6,13 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class O133 extends Applet {
+
     Button knop1, knop2;
+    boolean showMuurBaksteen = false;
+    boolean showMuurBeton = false;
 
     public void init() {
+
         knop1 = new Button("BaksteenMuur");
         knop2 = new Button("BetonMuur");
 
@@ -20,83 +24,80 @@ public class O133 extends Applet {
     }
 
     public void paint(Graphics g) {
-
-        tekenMuurBaksteen(g, 30, 30, 130, 100, 30, 170, 130, 240, 30, 310, 130, 380, 150, 40);
-        tekenMuurBeton(g, 30, 30, 130, 100, 30, 170, 130, 240, 30, 310, 130, 380, 180, 60);
+        if(showMuurBaksteen){
+            tekenMuurBaksteen(g, 30, 30, 130, 100, 30, 170, 130, 240, 30, 310, 130, 380, 150, 40);
+        }
+        if (showMuurBeton){
+            tekenMuurBeton(g, 30, 30, 130, 100, 30, 170, 130, 240, 30, 310, 130, 380, 180, 60);
+        }
 
     }
 
     public void tekenMuurBaksteen (Graphics g, int x1, int y1, int x2, int y2, int x3, int y3, int x4, int y4, int x5, int y5, int x6, int y6, int width, int height){
+
         setSize(1800, 500);
         setBackground(Color.gray);
-        for (int i = 0; i < 8; i++) {
-            g.setColor(Color.red);
-            g.fillRect(x1, y1, width, height);
-            x1 += 200;
-        } for (int i = 0; i < 8; i++) {
-            g.setColor(Color.red);
-            g.fillRect(x2, y2, width, height);
-            x2 += 200;
-        } for (int i = 0; i < 8; i++) {
-            g.setColor(Color.red);
-            g.fillRect(x3, y3, width, height);
-            x3 += 200;
-        } for (int i = 0; i < 8; i++) {
-            g.setColor(Color.red);
-            g.fillRect(x4, y4, width, height);
-            x4 += 200;
-        } for (int i = 0; i < 8; i++) {
-            g.setColor(Color.red);
-            g.fillRect(x5, y5, width, height);
-            x5 += 200;
-        }
-        for (int i = 0; i < 8; i++) {
-            g.setColor(Color.red);
-            g.fillRect(x6, y6, width, height);
-            x6 += 200;
+
+        int times = 6;
+        int x = x1;
+        int y = y1;
+
+        for (int current = 0; current < times; current++){
+            for (int i = 0; i < 8; i++) {
+                g.setColor(Color.red);
+                g.fillRect(x, y, width, height);
+                x += 200;
+            }
+            if (current % 2 == 0){
+                x = 0;
+            } else {
+                x = 50;
+            }
+            y += 70;
         }
 
     }
 
     public void tekenMuurBeton (Graphics g, int x1, int y1, int x2, int y2, int x3, int y3, int x4, int y4, int x5, int y5, int x6, int y6, int width, int height){
-        setBackground(Color.WHITE);
-        for (int i = 0; i < 8; i++) {
-            g.setColor(Color.gray);
-            g.fillRect(x1, y1, width, height);
-            x1 += 200;
-        } for (int i = 0; i < 8; i++) {
-            g.setColor(Color.gray);
-            g.fillRect(x2, y2, width, height);
-            x2 += 200;
-        } for (int i = 0; i < 8; i++) {
-            g.setColor(Color.gray);
-            g.fillRect(x3, y3, width, height);
-            x3 += 200;
-        } for (int i = 0; i < 8; i++) {
-            g.setColor(Color.gray);
-            g.fillRect(x4, y4, width, height);
-            x4 += 200;
-        } for (int i = 0; i < 8; i++) {
-            g.setColor(Color.gray);
-            g.fillRect(x5, y5, width, height);
-            x5 += 200;
-        }
-        for (int i = 0; i < 8; i++) {
-            g.setColor(Color.gray);
-            g.fillRect(x6, y6, width, height);
-            x6 += 200;
-        }
 
+        setSize(1800, 500);
+        setBackground(Color.WHITE);
+
+        int times = 6;
+        int x = x1;
+        int y = y1;
+
+        for (int current = 0; current < times; current++) {
+            for (int i = 0; i < 8; i++) {
+                g.setColor(Color.gray);
+                g.fillRect(x, y, width, height);
+                x += 200;
+            }
+            if (current % 2 == 0){
+                x = 0;
+            } else {
+                x = 50;
+            }
+            y += 70;
+        }
     }
 
     class KnopListener implements ActionListener {
         public void actionPerformed(ActionEvent e) {
+
+            showMuurBaksteen = true;
+            showMuurBeton = false;
+            repaint();
 
         }
     }
 
     class Knop2Listener implements ActionListener {
         public void actionPerformed(ActionEvent e) {
+
+            showMuurBeton = true;
+            showMuurBaksteen = false;
+            repaint();
 
         }
     }
